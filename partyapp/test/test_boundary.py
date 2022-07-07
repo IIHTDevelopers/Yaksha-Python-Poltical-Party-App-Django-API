d1ee159c6dbd3ac32025d2efdaf5b1e9e3746ade
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-07-07 18:31:47 +0530

    Delete partyapp directory

--- partyapp/test/test_boundary.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.test import APITestCase
-from partyapp.test.TestUtils import TestUtils
-class PoliticalAPIBoundaryTest(APITestCase):
-    def test_boundary(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestBoundary",True,"boundary")
-        print("TestBoundary = Passed")
