4bd37e5970ecc1aefe11bcbfb278f62287c8f7b9
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-07-07 18:33:23 +0530

    Add files via upload

---
+++ partyapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+class PoliticalPartyModel(models.Model):
+    party_id =models.AutoField(primary_key=True)
+    party_name=models.CharField(max_length=20)
+    party_founder=models.CharField(max_length=100)
+    # def __str__(self):
+    #     return self.party_name
+
+class PoliticalLeaderModel(models.Model):
+    leader_id =models.AutoField(primary_key=True)
+    party_id =models.IntegerField()
+    candidate_name =models.CharField(max_length=20)
+    state_name =models.CharField(max_length=100)
+    # def __str__(self):
+    #     return self.leader_id
+
+class DevelopmentModel(models.Model):
+    development_id =models.AutoField(primary_key=True)
+    leader_id =models.IntegerField()
+    development_title =models.CharField(max_length=20)
+    development_activity  =models.CharField(max_length=100)
+    development_budget =models.DecimalField(decimal_places=2,max_digits=100)
+    development_state  =models.CharField(max_length=100)
+    development_activity_month=models.IntegerField()#max_length=12
+    development_activity_year=models.IntegerField() #max_length=2040
+    # def __str__(self):
+    #     return self.development_id
